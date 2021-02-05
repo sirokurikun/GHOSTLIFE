0c17f48369ca66237db77e4212dacf9da0f78888
ghost_46 <koya.s.greeeen.1125@outlook.jp>
2021-02-05 19:24:44 +0900

    Add files via upload

--- src/main/java/ghostlife/ghostlife/ghostlife.java
+++ src/main/java/ghostlife/ghostlife/ghostlife.java
@@ -1,26 +1,18 @@
 package ghostlife.ghostlife;
 
 import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.PlayerChatEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-
 import java.util.*;
 
 public final class ghostlife extends JavaPlugin implements Listener {
@@ -40,23 +32,6 @@ public final class ghostlife extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
-
-    @SuppressWarnings({"deprecation"})
-    @EventHandler
-    public void onPlayerChat(PlayerChatEvent e) {
-        Player p = e.getPlayer();
-        String name = p.getDisplayName();
-        if ((e.getMessage().contains("@wiki"))) {
-            e.setCancelled(true);
-            if (!CoolDown.isCoolDown(p.getUniqueId().toString())) {
-                String message = getConfig().getString("wikimessage");
-                this.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', "" + name + message));
-                CoolDown.startCoolDown(p.getUniqueId().toString());
-            } else {
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cクールダウン中です。"));
-            }
-        }
     }
 
     @SuppressWarnings({"deprecation"})
@@ -109,7 +84,7 @@ public final class ghostlife extends JavaPlugin implements Listener {
             return true;
         }
 
-        if (cmd.getName().equalsIgnoreCase("addCustomModel")) {
+        if (cmd.getName().equalsIgnoreCase("addCustomModel") || cmd.getName().equalsIgnoreCase("acm")) {
             if (!sender.hasPermission("set.op")) {
                 sender.sendMessage("コマンドを実行出来る権限がありません。");
                 return true;
@@ -133,47 +108,63 @@ public final class ghostlife extends JavaPlugin implements Listener {
             }
         }
 
-        if (cmd.getName().equalsIgnoreCase("sellmmgui")) {
+        if (cmd.getName().equalsIgnoreCase("sellmmgui") || cmd.getName().equalsIgnoreCase("smg")) {
             Inventory mirror = Bukkit.createInventory(null,9,"§cSELLMMITEM MENU");
             ItemStack menu1 = new ItemStack(Material.GREEN_STAINED_GLASS);
             ItemStack menu2 = new ItemStack(Material.RED_STAINED_GLASS);
             ItemStack menu3 = new ItemStack(Material.YELLOW_STAINED_GLASS);
+            ItemStack menu4 = new ItemStack(Material.BLACK_STAINED_GLASS);
             ItemMeta itemMeta1 = menu1.getItemMeta();
             ItemMeta itemMeta2 = menu2.getItemMeta();
             ItemMeta itemMeta3 = menu3.getItemMeta();
+            ItemMeta itemMeta4 = menu4.getItemMeta();
             itemMeta1.setDisplayName(ChatColor.translateAlternateColorCodes('&',"&aSHOPを開く"));
             itemMeta2.setDisplayName(ChatColor.translateAlternateColorCodes('&',"&cSHOPを閉じる"));
             itemMeta3.setDisplayName(ChatColor.translateAlternateColorCodes('&',"&eSHOP注意点"));
-            List<String> lore = new ArrayList<String>();
-            lore.add(ChatColor.translateAlternateColorCodes('&',"&dSHOPのインベントリに"));
-            lore.add(ChatColor.translateAlternateColorCodes('&',"&d指定アイテム以外を入れてしまうと"));
-            lore.add(ChatColor.translateAlternateColorCodes('&',"&d一円にもならずアイテムが消えます"));
-            lore.add(ChatColor.translateAlternateColorCodes('&',"&d消えたアイテムに関しては&c補填対象外&dです"));
-            itemMeta3.setLore(lore);
+            itemMeta4.setDisplayName(ChatColor.translateAlternateColorCodes('&',"&8売却可能アイテム一覧をみる"));
+            List<String> lore3 = new ArrayList<String>();
+            lore3.add(ChatColor.translateAlternateColorCodes('&',"&dSHOPのインベントリに"));
+            lore3.add(ChatColor.translateAlternateColorCodes('&',"&d指定アイテム以外を入れてしまうと"));
+            lore3.add(ChatColor.translateAlternateColorCodes('&',"&d一円にもならずアイテムが消えます"));
+            lore3.add(ChatColor.translateAlternateColorCodes('&',"&d消えたアイテムに関しては&c補填対象外&dです"));
+            itemMeta3.setLore(lore3);
+            List<String> lore4 = new ArrayList<String>();
+            lore4.add(ChatColor.translateAlternateColorCodes('&',"&d売却可能アイテムは"));
+            lore4.add(ChatColor.translateAlternateColorCodes('&',"&3一万円&f,&3五千円&f"));
+            lore4.add(ChatColor.translateAlternateColorCodes('&',"&3二千円&f,&3千円&f,&6&l硬貨"));
+            lore4.add(ChatColor.translateAlternateColorCodes('&',"&7洞窟の欠片&f,&0&l黒曜石"));
+            lore4.add(ChatColor.translateAlternateColorCodes('&',"&f圧縮小麦チケット,圧縮ポテトチケット,圧縮人参チケット"));
+            itemMeta4.setLore(lore4);
             menu1.setItemMeta(itemMeta1);
             menu2.setItemMeta(itemMeta2);
             menu3.setItemMeta(itemMeta3);
+            menu4.setItemMeta(itemMeta4);
             mirror.setItem(0,menu1);
             mirror.setItem(8,menu2);
-            mirror.setItem(4,menu3);
+            mirror.setItem(5,menu3);
+            mirror.setItem(3,menu4);
             p.openInventory(mirror);
         }
         return true;
     }
 
     @EventHandler
-    public void onClick(InventoryClickEvent e){
+    public void onClick(InventoryClickEvent e) {
         Player player = (Player) e.getView().getPlayer();
         ItemStack slot = e.getCurrentItem();
-        if(slot == null) return;
-        if(e.getView().getTitle().equals(ChatColor.translateAlternateColorCodes('&',"&cSELLMMITEM MENU"))){
-            if(slot.getType() == Material.GREEN_STAINED_GLASS){
-               Inventory mirror = Bukkit.createInventory(null, 36, "§cSELLMMITEM SHOP");
-               player.openInventory(mirror);
-            }else if(slot.getType() == Material.RED_STAINED_GLASS){
-                player.closeInventory();
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&cSELLMMSHOP&fを閉じました"));
-            }else {
+        if (slot == null) return;
+        if (e.getView().getTitle().equals(ChatColor.translateAlternateColorCodes('&', "&cSELLMMITEM MENU"))) {
+            if (slot.getType() == Material.GREEN_STAINED_GLASS) {
+                if (slot.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&aSHOPを開く"))) {
+                    Inventory mirror = Bukkit.createInventory(null, 36, "§cSELLMMITEM SHOP");
+                    player.openInventory(mirror);
+                }
+            }else if (slot.getType() == Material.RED_STAINED_GLASS) {
+                if (slot.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&cSHOPを閉じる"))) {
+                    player.closeInventory();
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cSELLMMSHOP&fを閉じました"));
+                }
+            } else {
                 e.setCancelled(true);
             }
         }
@@ -182,15 +173,15 @@ public final class ghostlife extends JavaPlugin implements Listener {
     @EventHandler
     public void onClose(InventoryCloseEvent e){
         Inventory backpack = e.getInventory();
-        if(e.getView().getTitle().equals(ChatColor.translateAlternateColorCodes('&',"&cSELLMMITEM SHOP"))){
+        if (e.getView().getTitle().equals(ChatColor.translateAlternateColorCodes('&', "&cSELLMMITEM SHOP"))) {
             ItemStack[] contents = backpack.getContents();
             List<String> itemDisplayNameList = new ArrayList<>();
             double totalMoney = 0;
             for (String key : getConfig().getConfigurationSection("mmitem").getKeys(false)) {
                 int moneyamount = getConfig().getInt("mmitem." + key + ".sellprice");
-                for (int i = 0; i < 27; i++) {
+                for (int i = 0; i < 36; i++) {
                     ItemStack content = contents[i];
-                    if(content == null){
+                    if (content == null) {
                         continue;
                     }
                     int amount = content.getAmount();
